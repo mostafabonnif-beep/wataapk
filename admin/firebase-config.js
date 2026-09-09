2b86fa7ec291b5e1702ee2c19343852a8b2c884a
zocomputer <help@zocomputer.com>
2026-09-09 01:20:25 +0000

    fix: include Firebase web configuration for hosting build

---
+++ admin/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyADfeuW0OGnl2vT8WP2Q4VO2VmL8bLA1Pg",
+  authDomain: "elwataniatvapp.firebaseapp.com",
+  projectId: "elwataniatvapp",
+  storageBucket: "elwataniatvapp.firebasestorage.app",
+  messagingSenderId: "954896144400",
+  appId: "1:954896144400:web:3f18dfd4c60843def69295",
+  measurementId: "G-WQT4S9JRL7",
+  databaseURL: ""
+};
+
+window.__WATANIA_FIREBASE_CONFIG__ = Object.freeze({ ...firebaseConfig });
